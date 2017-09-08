b0407c29f552a8d6a3516604a3ff7115d7d96afe
phasenraum2010 <thomas@woehlke.org>
2017-09-08 09:14:25 +0200

    collecting data

--- src/test/java/org/woehlke/btw17/kandidaten/ExportKandidatTable.java
+++ src/test/java/org/woehlke/btw17/kandidaten/ExportKandidatTable.java
@@ -1,5 +1,6 @@
 package org.woehlke.btw17.kandidaten;
 
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.woehlke.btw17.kandidaten.configuration.KandidatenProperties;
 import org.woehlke.btw17.kandidaten.oodm.model.Kandidat;
-import org.woehlke.btw17.kandidaten.oodm.service.KandidatService;
+import org.woehlke.btw17.kandidaten.oodm.service.*;
+import org.woehlke.btw17.kandidaten.support.oodm.service.JdbcService;
 
 import java.io.*;
 import java.util.concurrent.Executors;
@@ -27,7 +30,44 @@ public class ExportKandidatTable {
     private final static String sqlFileKandidatData = "etc/3nf/data/insert-data-kandidat.sql";
 
     @Autowired
+    private BerufService berufService;
+
+    @Autowired
+    private BerufsgruppeService berufsgruppeService;
+
+    @Autowired
+    private BundeslandService bundeslandService;
+
+    @Autowired
+    private GeburtsortService geburtsortService;
+
+    @Autowired
+    private KandidatFlatService kandidatFlatService;
+
+    @Autowired
     private KandidatService kandidatService;
+
+    @Autowired
+    private ListeParteiService listeParteiService;
+
+    @Autowired
+    private ParteiService parteiService;
+
+    @Autowired
+    private LandesListeService landesListeService;
+
+    @Autowired
+    private WahlkreisService wahlkreisService;
+
+    @Autowired
+    private WohnortService wohnortService;
+
+    @Autowired
+    private JdbcService jdbcService;
+
+    @Autowired
+    private KandidatenProperties kandidatenProperties;
+
 
 
     @Commit
@@ -65,6 +105,43 @@ public class ExportKandidatTable {
         Executors.newSingleThreadExecutor().submit(streamGobbler);
         int exitCode = process.waitFor();
         assert exitCode == 0;
+    }
+
+    @Commit
+    @Test
+    public void build300CheckCount() throws Exception {
+        long countBerufSoll = kandidatenProperties.getTableContent().getCountBeruf();
+        long countBerufsgruppeSoll = kandidatenProperties.getTableContent().getCountBerufsgruppe();
+        long countBundeslandSoll = kandidatenProperties.getTableContent().getCountBundesland();
+        long countGeburtsortSoll = kandidatenProperties.getTableContent().getCountGeburtsort();
+        long countParteiSoll = kandidatenProperties.getTableContent().getCountPartei();
+        long countWahlkreisSoll = kandidatenProperties.getTableContent().getCountWahlkreis();
+        long countWohnortSoll = kandidatenProperties.getTableContent().getCountWohnort();
+        long countLandesListeSoll = kandidatenProperties.getTableContent().getCountLandesListe();
+        long countKandidatFlatSoll = kandidatenProperties.getTableContent().getCountKandidatFlat();
+        long countKandidatSoll = kandidatenProperties.getTableContent().getCountKandidat();
+
+        long countBerufIst = berufService.count();
+        long countBerufsgruppeIst = berufsgruppeService.count();
+        long countBundeslandIst = bundeslandService.count();
+        long countGeburtsortIst = geburtsortService.count();
+        long countParteiIst = parteiService.count();
+        long countWahlkreisIst = wahlkreisService.count();
+        long countWohnortIst = wohnortService.count();
+        long countLandesListeIst = landesListeService.count();
+        long countKandidatFlatIst = kandidatFlatService.count();
+        long countKandidatIst = kandidatService.count();
+
+        Assert.assertEquals(countBerufSoll,countBerufIst);
+        Assert.assertEquals(countBerufsgruppeSoll,countBerufsgruppeIst);
+        Assert.assertEquals(countBundeslandSoll,countBundeslandIst);
+        Assert.assertEquals(countGeburtsortSoll,countGeburtsortIst);
+        Assert.assertEquals(countParteiSoll,countParteiIst);
+        Assert.assertEquals(countWahlkreisSoll,countWahlkreisIst);
+        Assert.assertEquals(countWohnortSoll,countWohnortIst);
+        Assert.assertEquals(countLandesListeSoll,countLandesListeIst);
+        Assert.assertEquals(countKandidatFlatSoll,countKandidatFlatIst);
+        Assert.assertEquals(countKandidatSoll,countKandidatIst);
     }
 
     private static class StreamGobbler implements Runnable {
