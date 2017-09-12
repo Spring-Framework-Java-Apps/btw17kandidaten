1e73c6f59aa03a73cf7c44e264a9656d10de094b
phasenraum2010 <thomas@woehlke.org>
2017-09-12 11:58:29 +0200

    fixed #43

--- etc/export-kandidat.sh
+++ etc/export-kandidat.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#pg_dump -U kandidaten --table=kandidat --data-only --column-inserts kandidaten > 3nf/data/insert-data-kandidat.sql
+pg_dump -U kandidaten --table=kandidat --data-only --column-inserts kandidaten > 3nf/data/insert-data-kandidat.sql
 
-pg_dump -U kandidaten --table=liste_partei --data-only --column-inserts kandidaten > 3nf/data/insert-data-liste_partei.sql
+#pg_dump -U kandidaten --table=liste_partei --data-only --column-inserts kandidaten > 3nf/data/insert-data-liste_partei.sql
 
-#./build-db-data-import-file.sh
+./build-db-data-import-file.sh
 
 exit 0
