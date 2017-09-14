b15eabbbb45a50d06f5f4ab461e9240a9e232813
phasenraum2010 <thomas@woehlke.org>
2017-09-14 19:35:49 +0200

    fixed #110

--- src/main/java/org/woehlke/btw17/kandidaten/configuration/PageSymbol.java
+++ src/main/java/org/woehlke/btw17/kandidaten/configuration/PageSymbol.java
@@ -19,7 +19,7 @@ public enum PageSymbol {
     SOUNDCLOUD("<i class=\"fa fa-soundcloud\" aria-hidden=\"true\"></i>"),
     YOUTUBE("<i class=\"fa fa-youtube\" aria-hidden=\"true\"></i>"),
     FACEBOOK("<i class=\"fa fa-facebook-square\" aria-hidden=\"true\"></i>"),
-    WEBSEITE("<i class=\"fa fa-cloud\" aria-hidden=\"true\"></i>"),
+    WEBSEITE("<i class=\"fa fa-link\" aria-hidden=\"true\"></i>"),
     WIKIPEDIA("<i class=\"fa fa-wikipedia-w\" aria-hidden=\"true\"></i>"),
     STARTSEITE("<i class=\"fa fa-play-circle\" aria-hidden=\"true\"></i>"),
     GEBURTSJAHR("<i class=\"fa fa-id-card\" aria-hidden=\"true\"></i>"),
