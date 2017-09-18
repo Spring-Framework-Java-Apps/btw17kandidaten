cd3a677380fff9577b848f4f1187325cb574ae98
phasenraum2010 <thomas@woehlke.org>
2017-09-18 17:40:49 +0200

    Major refactoring with Database Schema Evolution, working on #113 #114 #115

---
+++ etc/3nf/data/insert-data-cms.sql
@@ -0,0 +1,28 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.5
+-- Dumped by pg_dump version 9.6.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: cms; Type: TABLE DATA; Schema: public; Owner: kandidaten
+--
+
+
+
+--
+-- PostgreSQL database dump complete
+--
+
