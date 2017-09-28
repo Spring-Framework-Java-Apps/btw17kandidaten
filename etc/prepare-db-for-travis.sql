07853f65a5b873b30ae7439975c6e298227e8884
phasenraum2010 <thomas@woehlke.org>
2017-09-28 10:30:13 +0200

    fixed #182, fixed #179

--- etc/prepare-db-for-travis.sql
+++ etc/prepare-db-for-travis.sql
@@ -2,18 +2,7 @@ DROP DATABASE kandidatentest;
 DROP USER kandidatentest;
 DROP ROLE kandidatentest;
 
-CREATE USER kandidatentest WITH PASSWORD 'kandidatentestpwd'
-    LOGIN
-    SUPERUSER
-    INHERIT
-    CREATEDB
-    CREATEROLE
-    NOREPLICATION;
-
+CREATE USER kandidatentest WITH PASSWORD 'kandidatentestpwd';
 GRANT pg_signal_backend, postgres TO kandidatentest WITH ADMIN OPTION;
 
-CREATE DATABASE kandidatentest WITH OWNER = kandidatentest
-ENCODING = 'UTF8'
-LC_COLLATE = 'de_DE.UTF-8'
-LC_CTYPE = 'de_DE.UTF-8'
-CONNECTION LIMIT = -1;
+CREATE DATABASE kandidatentest WITH OWNER = kandidatentest;
