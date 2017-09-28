34f89ab25bded525ab8a91bcd22572a2b833c5a1
phasenraum2010 <thomas@woehlke.org>
2017-09-28 11:10:01 +0200

    working on #179 build on travis is broken

--- travis.sh
+++ travis.sh
@@ -12,7 +12,14 @@ export BTW17_TWITTER_CARD_SITE=port80guru
 export BTW17_TWITTER_CARD_CREATOR=port80guru
 
 psql -U kandidatentest < etc/drop-tables.sql
-psql -U postgres < etc/prepare-db-for-travis.sql
+
+psql -c 'DROP DATABASE kandidatentest;' -U postgres
+psql -c 'DROP USER kandidatentest;' -U postgres
+psql -c 'DROP ROLE kandidatentest;' -U postgres
+psql -c "CREATE USER kandidatentest WITH PASSWORD 'kandidatentestpwd' LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE NOREPLICATION;" -U postgres
+psql -c 'GRANT pg_signal_backend, postgres TO kandidatentest WITH ADMIN OPTION;' -U postgres
+psql -c "CREATE DATABASE kandidatentest WITH OWNER = kandidatentest ENCODING = 'UTF8' LC_COLLATE = 'de_DE.UTF-8' LC_CTYPE = 'de_DE.UTF-8' CONNECTION LIMIT = -1;" -U postgres
+
 psql -c 'select * from version();' -U kandidatentest
 psql -c '\l' -U postgres
 psql -c '\dg' -U postgres
