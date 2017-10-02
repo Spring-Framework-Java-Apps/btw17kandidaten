4778f9b1be164e2b9ad167ce1974ee87771965a5
phasenraum2010 <thomas@woehlke.org>
2017-10-02 17:23:36 +0200

    fixed #121, fixed #120, fixed #127

--- etc/empty-tables-postgres.sql
+++ etc/empty-tables-postgres.sql
@@ -1,3 +1,3 @@
-TRUNCATE kandidat_ministerium, kandidat_ausschuss, ministerium, kandidat, kandidat_flat,landesliste,beruf,berufsgruppe,bundesland,geburtsort,listepartei,partei,wahlkreis,wohnort,fraktion,ausschuss,ministerium,kandidat_agentur,bundesland_agentur,listepartei_agentur,partei_agentur,wohnort_agentur,fraktion_agentur,ministerium_agentur,agentur,cms RESTART IDENTITY CASCADE;
+TRUNCATE kandidat_ministerium, kandidat_ausschuss, ministerium, kandidat, kandidat_flat,landesliste,beruf,berufsgruppe,bundesland,geburtsort,listepartei,partei,wahlkreis,wohnort,fraktion,ausschuss,ministerium,kandidat_agentur,bundesland_agentur,listepartei_agentur,partei_agentur,wohnort_agentur,fraktion_agentur,ministerium_agentur,agentur,cms,btw17_ergebnis,btw17_kandidat_flat,btw17_strukturdaten,btw17_wahlbewerber,btw17_wahlkreis RESTART IDENTITY CASCADE;
 -- TRUNCATE spring_session_attributes,spring_session RESTART IDENTITY CASCADE;
 ALTER SEQUENCE IF EXISTS hibernate_sequence RESTART;
