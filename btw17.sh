46bfc35f5c962096b08e6a787b160514536baec9
phasenraum2010 <thomas@woehlke.org>
2017-10-02 08:24:49 +0200

    fixed #122

--- btw17.sh
+++ btw17.sh
@@ -27,7 +27,7 @@ psql -U kandidatentest < etc/drop-tables.sql
 
 ./mvnw clean install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
 #./mvnw clean test -Ptravis -Dtest=Btw17WahlleiterDatenimport -Dmaven.javadoc.skip=true -B -V
-./mvnw clean test -Ptravis -Dtest=Btw17WahlkreisImporter -Dmaven.javadoc.skip=true -B -V
+./mvnw clean test -Ptravis -Dtest=Btw17WahlbewerberImporter -Dmaven.javadoc.skip=true -B -V
 
 
 
