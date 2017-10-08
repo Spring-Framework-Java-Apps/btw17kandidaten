57b103cff31a0f0f091d7184c2f79bca887970b9
phasenraum2010 <thomas@woehlke.org>
2017-10-08 04:00:45 +0200

    fixed #195

--- src/test/java/org/woehlke/btw17/kandidaten/oodm/bundeswahlleiter/enrich/BerufEnricher.java
+++ src/test/java/org/woehlke/btw17/kandidaten/oodm/bundeswahlleiter/enrich/BerufEnricher.java
@@ -23,7 +23,6 @@ import org.woehlke.btw17.kandidaten.configuration.spring.HttpSessionConfig;
 import org.woehlke.btw17.kandidaten.configuration.spring.WebMvcConfig;
 import org.woehlke.btw17.kandidaten.configuration.spring.WebSecurityConfig;
 import org.woehlke.btw17.kandidaten.oodm.model.Beruf;
-import org.woehlke.btw17.kandidaten.oodm.model.Wohnort;
 import org.woehlke.btw17.kandidaten.oodm.model.bundeswahlleiter.Btw17Wahlbewerber;
 import org.woehlke.btw17.kandidaten.oodm.service.*;
 import org.woehlke.btw17.kandidaten.support.oodm.service.JdbcService;
