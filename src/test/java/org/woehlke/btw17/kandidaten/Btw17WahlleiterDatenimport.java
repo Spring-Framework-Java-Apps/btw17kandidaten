ed8aa39100761c80563550e11d30421a20f6fdda
phasenraum2010 <thomas@woehlke.org>
2017-10-12 15:51:20 +0200

    fixed #215

--- src/test/java/org/woehlke/btw17/kandidaten/Btw17WahlleiterDatenimport.java
+++ src/test/java/org/woehlke/btw17/kandidaten/Btw17WahlleiterDatenimport.java
@@ -2,24 +2,19 @@ package org.woehlke.btw17.kandidaten;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.woehlke.btw17.kandidaten.oodm.importer.enrich.BundeslandEnricher;
-import org.woehlke.btw17.kandidaten.oodm.importer.file2db.Btw17ErgebnisImporter;
+import org.woehlke.btw17.kandidaten.oodm.importer.enrich.*;
 
 @Suite.SuiteClasses({
 
+        BerufEnricher.class,
+        //BundeslandEnricher.class,
+        //GeburtsortEnricher.class,
+        //ParteiEnricher.class,
+        //WahlkreisEnricher.class,
+        //WahlperiodeEnricher.class,
+        //WohnortEnricher.class,
+        //KandidatEnricher.class
 
-
-    /*
-    Btw17KandidatFlatServiceTest.class,
-    Btw17WahlkreisServiceTest.class,
-    Btw17StrukturdatenServiceTest.class,
-    Btw17WahlbewerberServiceTest.class,
-
-    Btw17ErgebnisImporter.class,
-    Btw17StrukturdatenImporter.class,
-    Btw17WahlbewerberImporter.class,
-    Btw17WahlkreisImporter.class
-    */
 })
 @RunWith(Suite.class)
 public class Btw17WahlleiterDatenimport {
