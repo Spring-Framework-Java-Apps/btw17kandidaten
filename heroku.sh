61f7e92b0b1f674461cbdb6e4d12b2511e89e028
a_total_another_bla <some@email.org>
2018-05-28 19:51:59 +0200

    setup and prepare for migration to spring-boot 2

--- heroku.sh
+++ heroku.sh
@@ -3,7 +3,7 @@
 export PORT=8080
 export JAVA_OPTS='-Xmx350m -Xss512k'
 
-export DATABASE_URL="jdbc:postgresql://localhost:5432/kandidaten?user=kandidaten&password=kandidatenpwd&schmema=public"
+export DATABASE_URL="jdbc:postgresql://localhost:5432/kandidatenheroku?user=kandidaten&password=kandidatenpwd&schmema=public"
 export BTW17_LOGIN_USERNAME=blubb
 export BTW17_LOGIN_PASSWORD=blablablablablablablablabla
 export BTW17_JPA_HIBERNATE_DDL_AUTO=update
