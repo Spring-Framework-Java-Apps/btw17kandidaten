598ef286518a3a9e7e89c1715edfaa83ddaeae0b
phasenraum2010 <thomas@woehlke.org>
2018-06-01 09:35:40 +0200

    migration to spring-boot 2

--- release.sh
+++ release.sh
@@ -4,6 +4,8 @@ source conf.sh
 
 export DATABASE_URL='jdbc:postgresql://localhost:5432/kandidatenrelease?user=kandidatenrelease&password=kandidatenreleasepwd'
 
+export BTW17_JPA_HIBERNATE_DDL_AUTO=update
+
 ./mvnw clean install
 
 ./mvnw clean spring-boot:run
