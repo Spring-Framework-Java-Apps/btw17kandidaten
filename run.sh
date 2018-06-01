af3a012192d8a0b8072bd8ff64214e530cfac6b8
a_total_another_bla <some@email.org>
2018-06-01 07:27:39 +0200

    Merge branch 'springboot2' of https://github.com/phasenraum2010/btw17kandidaten into springboot2
    # Conflicts:
    #	src/main/java/org/woehlke/btw17/kandidaten/oodm/model/Btw17Mdb.java

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
+source conf.sh
+
 export JAVA_OPTIONS='-Xmx300m -Xss512k -Dfile.encoding=UTF-8'
 
 export DATABASE_URL='jdbc:postgresql://localhost:5432/kandidaten?user=kandidaten&password=kandidatenpwd'
-
-source conf.sh
 
 export BTW17_JPA_HIBERNATE_DDL_AUTO=update
 
