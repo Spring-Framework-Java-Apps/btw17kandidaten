e4195731320d57990fe857b24c58babfab67477d
a_total_another_bla <some@email.org>
2018-06-08 14:40:31 +0200

    OGM - Neo4J Graph Datenmodell

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+source conf.sh
+
+export DATASOURCE_URL='jdbc:postgresql://localhost:5432/kandidatentest?user=kandidatentest&password=kandidatentestpwd'
+
+export DATASOURCE_USERNAME=kandidatentest
+export DATASOURCE_PASSWORD=kandidatentestpwd
+
+export BTW17_JPA_HIBERNATE_DDL_AUTO=update
+
+./mvnw -v -e clean install dependency:tree site  -B -V
+
+exit 0
